8e2c28e6a74845ac5590afe018b0dbead188a5be
kkimna0101 <kkimna0101@gmail.com>
2026-03-23 20:25:42 +0900

    fix(vercel): correct case-sensitive imports and scss variables

--- src/components/main/Transitionsection.jsx
+++ src/components/main/Transitionsection.jsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import './TransitionSection.scss';
+import './Transitionsection.scss';
 
 gsap.registerPlugin(ScrollTrigger);
 
