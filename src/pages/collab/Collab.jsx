16859c0b343058516ecd9252951f61288ab1d0b9
uiuxjy999 <uiuxjy999@gmail.com>
2026-03-25 12:43:47 +0900

    Remove temporary K-Brand banner from Collab page

--- src/pages/collab/Collab.jsx
+++ src/pages/collab/Collab.jsx
@@ -11,14 +11,6 @@ const Collab = () => {
       <CollabHero />
       <CollabShowcase />
       <CollabCollection />
-      <section className="collab-banner">
-        <Link to="/k-brand" className="kbrand-banner-link">
-          <div className="banner-content">
-            <h3>K-BRAND COLLECTION</h3>
-            <p>포터코리아에서만 만나볼 수 있는 특별한 큐레이션</p>
-          </div>
-        </Link>
-      </section>
     </div>
   );
 };
